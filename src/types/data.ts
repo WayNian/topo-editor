7083cae06f602ab30644f281b9360f43c67b8256
waynian <waynian@foxmail.com>
2023-12-07 22:29:56 +0800

    feat: 完善类型

--- src/types/data.ts
+++ src/types/data.ts
@@ -6,20 +6,20 @@ export interface ISourceNode {
   nodePosition: string;
   nodeSize: string;
   rotate: number;
-  nodeStyles: string;
+  nodeStyles: string | Record<string, string>;
   nodeText: string;
   fontSize: string;
   fontColor: string;
   textPosition: string;
-  textStyles: string;
-  bindData: any;
-  bindMap: any;
+  textStyles: string | Record<string, string>;
+  bindData: Record<string, string>;
+  bindMap: string;
   bindLink: any;
-  bindSubLink: any;
-  metaData: any;
-  updatedBy: any;
-  updatedTime: any;
-  sublayerList: any;
+  bindSubLink: string;
+  metaData: Record<string, string>;
+  updatedBy: string;
+  updatedTime: string;
+  sublayerList: string[];
 }
 
 export interface INode extends ISourceNode {
@@ -27,6 +27,30 @@ export interface INode extends ISourceNode {
   y: number;
   width: number;
   height: number;
-  nodeStyles: any;
-  textStyles: any;
+  nodeStyles: Record<string, string>;
+  textStyles: Record<string, string>;
+}
+
+export interface ISourceLink {
+  linkId: string;
+  mapId: string;
+  linkType: string;
+  dashedLink: string;
+  compClass: string;
+  linkPath: string;
+  linkWidth: number;
+  linkStyles: string | Record<string, string>;
+  linkAnimations: Record<string, string>;
+  fromObj: string;
+  endObj: string;
+  bindData: Record<string, string>;
+  bindMap: string;
+  metaData: Record<string, string>;
+  updatedBy: string;
+  updatedTime: string;
+  sublayerList: string[];
+}
+
+export interface ILink extends ISourceLink {
+  linkStyles: Record<string, string>;
 }
