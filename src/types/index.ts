ba642df81c9e8c23fec655e6f51dec866ea5cab0
waynian <waynian@foxmail.com>
2024-03-12 14:49:53 +0800

    feat: 新增axios

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
 export * from "./svg";
 export * from "./mitt";
 export * from "./data";
+export * from "./http";
+export * from "./system";
---
+++ src/utils/http/code.ts
@@ -0,0 +1,13 @@
+// set code cofig
+export enum CodeConfig {
+  success = 200,
+  notFound = 404,
+  noPermission = 403
+}
+
+// set code cofig
+export enum HttpCodeConfig {
+  success = 200,
+  notFound = 404,
+  noPermission = 403
+}
