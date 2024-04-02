ba4c61f2fb2d8a4a4f00116e83ede995fe6cf355
waynian <waynian@foxmail.com>
2024-04-02 14:16:48 +0800

    feat: 优化头部图标显示

--- src/components/SvgEditor/Header/index.vue
+++ src/components/SvgEditor/Header/index.vue
@@ -21,27 +21,55 @@
     </div>
     <!-- <div>操作</div> -->
     <div>
-      <n-button text style="font-size: 24px" class="mx-3">
-        <n-icon>
-          <OptionsSharp />
-        </n-icon>
-      </n-button>
-      <n-button text style="font-size: 24px">
-        <n-icon>
-          <SettingsSharp />
-        </n-icon>
-      </n-button>
+      <n-dropdown :options="exportOptions">
+        <n-button text style="font-size: 24px" class="mx-3">
+          <n-icon>
+            <FileExport />
+          </n-icon>
+        </n-button>
+      </n-dropdown>
+
+      <n-dropdown :options="settingOptions">
+        <n-button text style="font-size: 24px">
+          <n-icon>
+            <SettingsSharp />
+          </n-icon>
+        </n-button>
+      </n-dropdown>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ColorFilterSharp, LibrarySharp, OptionsSharp, SettingsSharp } from "@vicons/ionicons5";
+import { ColorFilterSharp, LibrarySharp, SettingsSharp } from "@vicons/ionicons5";
 import MenuSharp from "@/assets/images/icons/MenuSharp.svg?component";
+import FileExport from "@/assets/images/icons/FileExport.svg?component";
 
 import { useMenuStore } from "@/stores";
 
 const menuStore = useMenuStore();
+
+const exportOptions = [
+  {
+    label: "SVG",
+    key: "svg"
+  },
+  {
+    label: "PNG",
+    key: "png"
+  },
+  {
+    label: "PDF",
+    key: "PDF"
+  }
+];
+
+const settingOptions = [
+  {
+    label: "退出登录",
+    key: "logout"
+  }
+];
 
 const toggleCollapsed = () => {
   menuStore.collapsed = !menuStore.collapsed;
